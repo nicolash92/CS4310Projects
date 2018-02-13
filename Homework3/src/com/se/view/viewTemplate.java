3c00d4090007cdb16842c71c4fb04fa60e649873
DNM <DNM@derricks-mbp>
2018-02-12 17:42:14 -0800

    imports

--- Homework3/src/com/se/view/viewTemplate.java
+++ Homework3/src/com/se/view/viewTemplate.java
@@ -1,5 +1,14 @@
 package com.se.view;
 
-public abstract class viewTemplate {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public abstract class viewTemplate extends Application {
 
 }
