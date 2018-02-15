98c32827303fcc150745d5ce9be4bd5366d2b63e
DNM <DNM@derricks-mbp>
2018-02-14 20:58:52 -0800

    testerView

---
+++ Homework3/src/com/se/view/tests/ViewTester.java
@@ -0,0 +1,20 @@
+package com.se.view.tests;
+
+import java.util.Scanner;
+import com.se.view.viewReminders;
+
+public class ViewTester {
+	/*
+	public static void main(String[] args) {
+		
+		Scanner test = new Scanner(System.in);
+		viewReminders view = new viewReminders();
+		
+		System.out.println(view.menu(test));
+		System.out.println(view.add(test));
+		System.out.println(view.turns(test));
+	
+	}
+	*/
+
+}
